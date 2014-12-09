0d2ca5c23abbb5d865b8be98676650c7ba8f1ba6
Vasya Semenov <monstrodev@gmail.com>
2014-12-09 18:53:14 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngTradingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngTradingApp
+ */
+angular.module('ngTradingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
